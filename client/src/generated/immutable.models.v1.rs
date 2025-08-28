0421f093477c25fbb5ad1070c382eb3ae08a546c
Arnaud Brousseau <rno@turnkey.io>
2025-08-28 10:41:58 -0500

    Fix codegen and delete most of the private protos

--- client/src/generated/immutable.models.v1.rs
+++ client/src/generated/immutable.models.v1.rs
@@ -1,9 +1,17 @@
 #[derive(Debug)]
 #[serde_with::serde_as]
+#[derive(Debug)]
+#[serde_with::serde_as]
+#[derive(Debug)]
+#[serde_with::serde_as]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq)]
 pub struct MerkleNode {
+    #[serde(default)]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
+    #[serde(default)]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
     #[serde(default)]
     #[serde_as(as = "serde_with::DisplayFromStr")]
     pub id: u64,
@@ -11,8 +19,14 @@ pub struct MerkleNode {
     /// For a leaf node, this would be organization_digest from from the associated notarization.
     /// For a none-leaf node (i.e. an intermediate node or the root), this would be the computed digest of the children.
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub value: ::prost::alloc::vec::Vec<u8>,
 }
+#[derive(Debug)]
+#[serde_with::serde_as]
+#[derive(Debug)]
+#[serde_with::serde_as]
 #[derive(Debug)]
 #[serde_with::serde_as]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
@@ -20,22 +34,38 @@ pub struct MerkleNode {
 #[derive(Clone, PartialEq)]
 pub struct MerkleRootPayload {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub node: ::core::option::Option<MerkleNode>,
+    #[serde(default)]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
+    #[serde(default)]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
     #[serde(default)]
     #[serde_as(as = "serde_with::DisplayFromStr")]
     pub timestamp: u64,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq)]
 pub struct MerkleRoot {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub payload: ::core::option::Option<MerkleRootPayload>,
     /// Notarizer signature over hash(payload)
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub signature: ::core::option::Option<Signature>,
 }
+#[derive(Debug)]
+#[serde_with::serde_as]
+#[derive(Debug)]
+#[serde_with::serde_as]
 #[derive(Debug)]
 #[serde_with::serde_as]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
@@ -43,13 +73,21 @@ pub struct MerkleRoot {
 #[derive(Clone, PartialEq)]
 pub struct MerkleLeafPayload {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub node: ::core::option::Option<MerkleNode>,
+    #[serde(default)]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
+    #[serde(default)]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
     #[serde(default)]
     #[serde_as(as = "serde_with::DisplayFromStr")]
     pub timestamp: u64,
     /// The UUID of the organization
     pub organization_id: ::prost::alloc::string::String,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 /// NOTE: The MerkleLeaf itself needs to be signed, rather than simply relying on the signature of its assocaited Notarization.
 /// The purpose of this is to make the `MerkleLeafPayload.timestamp` immutable, so that it can be used by the Ump to verify the relative age of leaf and notarization.
@@ -58,33 +96,53 @@ pub struct MerkleLeafPayload {
 #[derive(Clone, PartialEq)]
 pub struct MerkleLeaf {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub payload: ::core::option::Option<MerkleLeafPayload>,
     /// Notarizer signature over hash(payload)
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub signature: ::core::option::Option<Signature>,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq)]
 pub struct MerkleProof {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub root: ::core::option::Option<MerkleRoot>,
+    #[serde(default)]
+    #[serde(default)]
     #[serde(default)]
     pub nodes: ::prost::alloc::vec::Vec<MerkleNode>,
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub leaves: ::prost::alloc::vec::Vec<MerkleLeaf>,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq)]
 pub struct MerkleLeafNotarization {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub leaf: ::core::option::Option<MerkleLeaf>,
+    #[serde(default)]
+    #[serde(default)]
     #[serde(default)]
     pub notarization: ::core::option::Option<Notarization>,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 /// The original version of AccountPayload used for signature verification.
 /// This version did not contain the `exported` or `public_key` fields.
@@ -102,6 +160,8 @@ pub struct AccountPayloadV0 {
     pub address: ::prost::alloc::string::String,
 }
 #[derive(Debug)]
+#[derive(Debug)]
+#[derive(Debug)]
 /// Introduced the `exported` field to indicate whether the account was externally exported.
 /// This broke compatibility with signatures generated using V0, so we retain both versions.
 #[derive(::serde::Serialize, ::serde::Deserialize)]
@@ -116,8 +176,12 @@ pub struct AccountPayloadV1 {
     pub address_format: super::super::common::v1::AddressFormat,
     pub address: ::prost::alloc::string::String,
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub exported: bool,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 /// Introduced `public_key` (optional) field to support embedding the public key.
 /// This is the canonical and current version for new signatures.
@@ -134,10 +198,16 @@ pub struct AccountPayloadV2 {
     pub address_format: super::super::common::v1::AddressFormat,
     pub address: ::prost::alloc::string::String,
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub exported: bool,
+    #[serde(default)]
+    #[serde(default)]
     #[serde(default)]
     pub public_key: ::core::option::Option<::prost::alloc::string::String>,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 /// The signed wrapper containing a payload (currently AccountPayloadV2)
 /// and its corresponding cryptographic signature.
@@ -147,11 +217,19 @@ pub struct AccountPayloadV2 {
 #[derive(Clone, PartialEq)]
 pub struct Account {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub payload: ::core::option::Option<AccountPayloadV2>,
     /// Signer signature over hash(payload)
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub signature: ::core::option::Option<Signature>,
 }
+#[derive(Debug)]
+#[serde_with::serde_as]
+#[derive(Debug)]
+#[serde_with::serde_as]
 #[derive(Debug)]
 #[serde_with::serde_as]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
@@ -166,6 +244,10 @@ pub struct NotarizationPayload {
     /// This timestamp is in ms, in UTC. It comes directly from the NSM.
     #[serde(default)]
     #[serde_as(as = "serde_with::DisplayFromStr")]
+    #[serde(default)]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
+    #[serde(default)]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
     pub timestamp: u64,
     /// The serialized organization version used to calculate the digest
     pub organization_data_version: ::prost::alloc::string::String,
@@ -173,16 +255,24 @@ pub struct NotarizationPayload {
     pub organization_id: ::prost::alloc::string::String,
 }
 #[derive(Debug)]
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq)]
 pub struct Notarization {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub payload: ::core::option::Option<NotarizationPayload>,
     /// Notarizer signature over hash(payload)
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub signature: ::core::option::Option<Signature>,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 /// while not technically immutable, this felt like the logical place to put this for now
 #[derive(::serde::Serialize, ::serde::Deserialize)]
@@ -190,12 +280,20 @@ pub struct Notarization {
 #[derive(Clone, PartialEq)]
 pub struct NotarizedOrganization {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub data_bytes: ::prost::alloc::vec::Vec<u8>,
+    #[serde(default)]
+    #[serde(default)]
     #[serde(default)]
     pub notarization: ::core::option::Option<Notarization>,
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub merkle_proof: ::core::option::Option<MerkleProof>,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -206,6 +304,8 @@ pub struct Signature {
     pub message: ::prost::alloc::string::String,
     pub signature: ::prost::alloc::string::String,
 }
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(Debug)]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -222,13 +322,19 @@ pub struct SmartContractInterfacePayload {
     pub notes: ::prost::alloc::string::String,
 }
 #[derive(Debug)]
+#[derive(Debug)]
+#[derive(Debug)]
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq)]
 pub struct SmartContractInterface {
     #[serde(default)]
+    #[serde(default)]
+    #[serde(default)]
     pub payload: ::core::option::Option<SmartContractInterfacePayload>,
     /// Signer signature over hash(payload)
+    #[serde(default)]
+    #[serde(default)]
     #[serde(default)]
     pub signature: ::core::option::Option<Signature>,
 }
@@ -236,24 +342,38 @@ pub struct SmartContractInterface {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum SignatureScheme {
     #[serde(rename = "SIGNATURE_SCHEME_UNSPECIFIED")]
+    #[serde(rename = "SIGNATURE_SCHEME_UNSPECIFIED")]
+    #[serde(rename = "SIGNATURE_SCHEME_UNSPECIFIED")]
     Unspecified = 0,
     /// Scheme used for Turnkey's public API
+    #[serde(rename = "SIGNATURE_SCHEME_TK_API_P256")]
+    #[serde(rename = "SIGNATURE_SCHEME_TK_API_P256")]
     #[serde(rename = "SIGNATURE_SCHEME_TK_API_P256")]
     TkApiP256 = 1,
     /// Scheme used on our UI when users sign with Webauthn
     /// Public keys are encoded using COSE (<https://www.w3.org/TR/webauthn-2/#sctn-encoded-credPubKey-examples>)
     #[serde(rename = "SIGNATURE_SCHEME_TK_WEBAUTHN")]
+    #[serde(rename = "SIGNATURE_SCHEME_TK_WEBAUTHN")]
+    #[serde(rename = "SIGNATURE_SCHEME_TK_WEBAUTHN")]
     TkWebauthn = 2,
     /// Scheme used by our enclave applications
+    #[serde(rename = "SIGNATURE_SCHEME_TK_QUORUM_P256")]
+    #[serde(rename = "SIGNATURE_SCHEME_TK_QUORUM_P256")]
     #[serde(rename = "SIGNATURE_SCHEME_TK_QUORUM_P256")]
     TkQuorumP256 = 3,
     /// Scheme used for Turnkey's public API
     #[serde(rename = "SIGNATURE_SCHEME_TK_API_SECP256K1")]
+    #[serde(rename = "SIGNATURE_SCHEME_TK_API_SECP256K1")]
+    #[serde(rename = "SIGNATURE_SCHEME_TK_API_SECP256K1")]
     TkApiSecp256k1 = 4,
     /// Scheme used for Turnkey's public API
     #[serde(rename = "SIGNATURE_SCHEME_TK_API_ED25519")]
+    #[serde(rename = "SIGNATURE_SCHEME_TK_API_ED25519")]
+    #[serde(rename = "SIGNATURE_SCHEME_TK_API_ED25519")]
     TkApiEd25519 = 5,
     /// Scheme used for Ethereum wallet signatures
+    #[serde(rename = "SIGNATURE_SCHEME_TK_API_SECP256K1_EIP191")]
+    #[serde(rename = "SIGNATURE_SCHEME_TK_API_SECP256K1_EIP191")]
     #[serde(rename = "SIGNATURE_SCHEME_TK_API_SECP256K1_EIP191")]
     TkApiSecp256k1Eip191 = 6,
 }
