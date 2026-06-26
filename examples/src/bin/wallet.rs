b04dded7343e67452e87287dd9e737dbfb73d147
Daniil Ruditskiy <daniil@turnkey.io>
2026-06-25 17:35:32 -0700

    chore(rust-sdk): sync protos
    - manual updates to satisfy compiler

--- examples/src/bin/wallet.rs
+++ examples/src/bin/wallet.rs
@@ -38,6 +38,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     path_format: PathFormat::Bip32,
                     path: "m/44'/60'/0'/0".to_string(),
                     address_format: AddressFormat::Ethereum,
+                    name: None,
                 }],
                 mnemonic_length: None, // Let that be the default
             },
