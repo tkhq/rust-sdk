9013c58dbc28e4ba1ee6934047d8ff52cf0a1760
Richard Pringle <richard@turnkey.io>
2026-07-16 11:06:49 -0400

    This is a commit message

--- tvc/src/main.rs
+++ tvc/src/main.rs
@@ -1,6 +1,8 @@
 use tracing::debug;
 use tvc::cli::Cli;
 
+compile_error!("I couldn't make the logic incorrect, I had to make the compiler error out");
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tvc::logging::init();
